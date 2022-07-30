7778d8b70de161f0f50bfc396b46daad975c0b21
yaena1223 <yaena1223@dgu.ac.kr>
2022-07-31 05:43:39 +0900

    effect

--- react_movie/src/App.js
+++ react_movie/src/App.js
@@ -1,12 +1,38 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword,setKeyword] = useState("")
+  const onClick = () => setValue((prev)=>prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
 
+  useEffect(()=>{
+    console.log("I run only once");
+  },[]);
+
+  useEffect(()=>{
+      console.log("I run when 'keyword' changes", keyword);
+    },[keyword]);
+
+  useEffect(()=>{
+      console.log("I run when 'counter' changes", keyword);
+    },[counter]);
+
+  useEffect(()=>{
+    console.log("I run when 'keyword & counter' changes", keyword);
+  },[keyword,counter]);
   return (
     <div>
-      <h1 className = {styles.title}>Welcome back!</h1>
-      <Button text="Continue"/>
+      <input 
+        type = "text" 
+        placeholder="Search here..." 
+        onChange = {onChange}
+        value = {keyword}
+        />
+      <h1 className = {styles.title}>{counter}</h1>
+      <button onClick = {onClick}>click me</button>
     </div>
   );
 }
